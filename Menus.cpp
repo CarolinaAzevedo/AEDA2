f7cad26749d83b9e4cc2a732b7f346c2bd38229e
eduardamsc <mariaeduardascunha97@gmail.com>
2016-11-18 00:09:36 +0000

    natal
    menus atualizados

--- Menus.cpp
+++ Menus.cpp
@@ -34,12 +34,10 @@ void Menu()
 	cout << "Hello and Welcome to The Voice's records" << endl;
 	cout << "As a member of the general public, you can access statistics and details on previous editions.";
 	cout << endl << "Which option do you want to consult?"<< endl;
+
 	cout << "1 - Who were the mentors?" << endl;
 	cout << "2 - Who were the contestants?" << endl;
-	cout << "3 - What songs were covered?" << endl;
-	cout << "4 - Who won?" << endl;
-	cout << "5 - Which mentor was the most successful?" << endl;
-	cout << "6 - Return to the login menu." << endl;
+	cout << "3 - Who won?";
 
 	AvailableInfo();
 }
@@ -82,10 +80,10 @@ void AvailableInfo()
 
 	}
 
-	while (option != 1 && option != 2 && option != 3 && option != 4 && option != 5 && option != 6)
+	while (option != 1 && option != 2 && option != 3)
 	{
 		cout << "Error! The option you've selected isn't available.";
-		cout << endl << "You must press 1, 2, 3, 4, 5 or 6! Try again." << endl;
+		cout << endl << "You must press 1, 2 or 3! Try again." << endl;
 		cin >> option;
 	}
 
@@ -95,38 +93,101 @@ void AvailableInfo()
 	case 1: //mentores
 		chooseSeason();
 
+		cout << "Which option do you want to consult?" << endl;
+
+		cout << "1 - What was their team?" << endl;
+		cout << "2 - Which was the most successful mentor?" << endl;
+
+		cout << "3 - Return to starting menu.";
+
 		break;
 
 	case 2: //concorrentes
 		chooseSeason();
 
+		cout << "Which option do you want to consult?"<< endl;
+
+		cout << "1 - Which songs were sung?" << endl;
+		cout << "2 - Which chairs were turned?" << endl;
+		cout << "3 - In which season were they eliminated?" << endl;
+
+		cout << "4 - Return to starting menu.";
 		break;
 
-	case 3: //musicas
+	case 3: //vencedores
 		chooseSeason();
 
 		break;
+	}
+}
 
-	case 4: //quem venceu
+void menuMentors()
+{
+	unsigned int option;
+	cin >> option;
+
+	while (cin.fail())
+	{
+		cout << "Error! You can type digits only! Try again." << endl;
+		cout << "Type in option again: ";
+		cin >> option;
+	}
+
+	while (option != 1 && option != 2 && option != 3) {
+		cout << "Error! The option you've selected isn't available.";
+		cout << endl << "You must press 1, 2 or 3! Try again." << endl;
+		cin >> option;
+	}
+
+	switch (option) {
+	case 1: //equipas
 		chooseSeason();
 
 		break;
-
-	case 5: //mentor com mais sucesso
-		chooseSeason();
+	case 2: //mais bem sucedido
 
 		break;
-
-	case 6: //Return to the login menu
+	case 3:
 		Menu();
 		break;
 	}
+}
 
-	/*para cada edição do concurso:
-	 * que cadeiras de mentores conseguiram virar,
-	 * quem foram os elementos de cada equipa
-	 * em que etapa foram eliminados,
-	 * que pontuações obtiveram do público*/
+void menuContestants()
+{
+	unsigned int option;
+	cin >> option;
+
+	while (cin.fail())
+	{
+		cout << "Error! You can type digits only! Try again." << endl;
+		cout << "Type in option again: ";
+		cin >> option;
+	}
+
+	while (option != 1 && option != 2 && option != 3 && option != 4) {
+		cout << "Error! The option you've selected isn't available.";
+		cout << endl << "You must press 1, 2, 3 or 4! Try again." << endl;
+		cin >> option;
+	}
+
+	switch (option) {
+	case 1: //musicas
+		chooseSeason();
+
+		break;
+	case 2: //cadeiras que virou
+		chooseSeason();
+
+		break;
+	case 3: //etapa em que foi eliminado
+		chooseSeason();
+
+		break;
+	case 4:
+		Menu();
+		break;
+	}
 }
 
 #endif /* SRC_MENUS_CPP_ */
